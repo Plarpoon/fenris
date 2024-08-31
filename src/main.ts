228eca43d7cdd993abfe2ed35c29d3074f10a9cb
Alessandro Suha <github@plarp.net>
2024-08-31 20:26:58 +0200

    refactor: lay down basic ui elements

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
-import { invoke } from "@tauri-apps/api/core";
+window.addEventListener("DOMContentLoaded", () => {
+  const settingsButton = document.querySelector("#settings-button");
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
+  if (settingsButton) {
+    settingsButton.addEventListener("click", () => {
+      alert("Settings button clicked!");
     });
   }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
 });
